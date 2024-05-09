80bbd1243425d15a0792f9b5a7bea4ff6758232e
Krisztina Russell-Elzer <elzerk4@gmail.com>
2024-05-09 16:22:41 +0100

    Task-001 Net 8 Core project with HttpRequest
    HttpRequestExtensions.cs created and you can check the solution on the view.

---
+++ Net8CoreSample/Extensions/HttpRequestExtensions.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+
+public static class HttpRequestExtensions
+{
+    public static string GetRawUrl(this HttpRequest request)
+    {
+        var httpContext = request.HttpContext;
+        return $"{httpContext.Request.Scheme}://{httpContext.Request.Host}{httpContext.Request.Path}{httpContext.Request.QueryString}";
+    }
+}
